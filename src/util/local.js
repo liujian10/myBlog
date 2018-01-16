50c8d3b7c2ea3ec9b4987deb4e12ba0429793d6d
liujian10 <liujian10@le.com>
2018-01-16 10:28:57 +0800

    Update cvInfo.json

--- src/util/local.js
+++ src/util/local.js
@@ -55,11 +55,11 @@ const setTTL = (key, ttl) => {
 
 const getTTL = key => storage[TTL][key] - (+new Date());
 
-// const localIp = '10.58.93.124'
+//export const localIp = '10.58.93.124';
 
 export const isLocal = () => {
   return true;
-  // location.href.indexOf('localhost') > -1 || location.href.indexOf(localIp) > -1
+  // location.href.indexOf('localhost') > -1 || location.href.indexOf(localIp) > -1;
 };
 
 export default { set, get, clear, setTTL, getTTL, isLocal };
